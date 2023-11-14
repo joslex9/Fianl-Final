d669b32d4b1f707eec632ae97573a594a0a1dd9a
joslex9 <134178860+joslex9@users.noreply.github.com>
2023-11-14 03:23:48 -0500

    Mensaje de confirmación descriptivo

---
+++ config/static/js/pdf.js
@@ -0,0 +1,23 @@
+const $elementoParaConvertir = document.body;
+
+html2pdf()
+    .set({
+        margin: 1,
+        filename: 'documento.pdf',
+        image: {
+            type: 'jpeg',
+            quality: 0.98
+        },
+        html2canvas: {
+            scale: 3, // A mayor escala, mejores gráficos, pero más peso
+            letterRendering: true,
+        },
+        jsPDF: {
+            unit: "in",
+            format: "a3",
+            orientation: 'portrait' // landscape o portrait
+        }
+    })
+    .from($elementoParaConvertir)
+    .save()
+    .catch(err => console.log(err));
